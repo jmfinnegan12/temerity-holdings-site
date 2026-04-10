e98939c905daf9445a7a94e5b56059e6c3da188f
Jim Finnegan <jmfinnegan12@gmail.com>
2026-04-10 12:34:08 -0700

    Quartz sync: Apr 10, 2026, 12:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jmfinnegan12.github.io/temerity-holdings-site",
+    baseUrl: "temerityhq.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
